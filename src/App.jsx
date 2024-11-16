5623e18324d4118ccd184cc03f5a0cabd6ccd53f
hadeel <hadeelhalliq@gmail.com>
2024-11-16 09:40:30 -0800

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -37,12 +37,19 @@ export default class App extends Component {
       <>
       <Routes>
         <Route index element={<Navigate to='/login'/>}/>
+        <Route index element={<Navigate to='/watches'/>}/>
         <Route path='/login' element={this.state.isAuthorized ? <Navigate to='/dashboard'/> : <LogIn login={this.login}/>}/>
+        <Route path='/watches' element={this.state.isAuthorized ? <Navigate to='/dashboard'/> : <LogIn login={this.login}/>}/>
         <Route path='/dashboard' element={<ProtectedRoute isAuthorized={this.state.isAuthorized} logout={this.logout}/>}/>
+        <Route path='/watches' element={<ProtectedRoute isAuthorized={this.state.isAuthorized} logout={this.logout}/>}/>
         <Route path='/dashboard/profile' element={<Profile handleIsAdminChange={this.handleIsAdminChange} />} />
+        <Route path='/watches' element={<Profile handleIsAdminChange={this.handleIsAdminChange} />} />
         <Route path='/profile/userslist' element={this.state.isAdmin ? <UsersList /> : <Navigate to='/dashboard/profile' />} />
+        <Route path='/watches' element={this.state.isAdmin ? <UsersList /> : <Navigate to='/dashboard/profile' />} />
         <Route path='/userslist/userdetailes/:id' element={<UserDetailes />}/>
+        <Route path='/watches' element={<UserDetailes />}/>
         <Route path='/register' element={<SignUp />}/>
+        <Route path='/watches' element={<SignUp />}/>
       </Routes>
       
       </>
